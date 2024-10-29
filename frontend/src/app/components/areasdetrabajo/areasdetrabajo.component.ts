25c6228046bb9c02f3928a820a5be355fbdab49a
Lazin3ss <laziness@protonmail.com>
2024-10-29 05:38:23 -0300

    frontend: Agregar funcionalidad de edición/eliminado para las áreas de trabajo
    Se puede editar in-line y eliminar items mediante botones que se encuentran dentro de las filas de la tabla.

--- frontend/src/app/components/areasdetrabajo/areasdetrabajo.component.ts
+++ frontend/src/app/components/areasdetrabajo/areasdetrabajo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { AreasDeTrabajoService } from '../../services/areasdetrabajo.service';
 import { EmpleadosService } from '../../services/empleados.service';
 
+
 @Component({
-  imports: [NgFor],
+  imports: [NgIf, NgFor],
   selector: 'app-areasdetrabajo',
   standalone: true,
   templateUrl: './areasdetrabajo.component.html',
@@ -12,6 +13,10 @@ import { EmpleadosService } from '../../services/empleados.service';
 })
 
 export class AreasDeTrabajoComponent {
+  editMode = false;
+  editId = -1;
+  nombre = '';
+
   constructor(
     private areasDeTrabajoService: AreasDeTrabajoService,
     private empleadosService: EmpleadosService
@@ -21,7 +26,40 @@ export class AreasDeTrabajoComponent {
     return this.areasDeTrabajoService.getAllAreasDeTrabajo();
   }
 
+  getAreaDeTrabajoById(id: number) {
+    return this.areasDeTrabajoService.getAreaDeTrabajoById(id);
+  }
+
   getAreaDeTrabajoTotalEmployees(id: number) {
     return this.empleadosService.getEmpleadosByAreaDeTrabajoId(id).length;
+  }
+
+  removeAreaDeTrabajo(id: number) {
+    this.areasDeTrabajoService.removeById(id);
+  }
+
+  startEditMode(id: number) {
+    this.editMode = true;
+    this.nombre = '';
+    if (id != null) {
+      this.editId = id;
+      const area = this.getAreaDeTrabajoById(id);
+      if (area != null) {
+        this.nombre = area.nombre;
+      }
+    }
+  }
+
+  editing(id: number) {
+    return this.editId == id;
+  }
+
+  endEditMode(submit: boolean) {
+    if (submit) {
+      this.areasDeTrabajoService.updateAreaDeTrabajo(this.editId, this.nombre);
+    }
+    this.editMode = false;
+    this.nombre = '';
+    this.editId = -1;
   }
 }
--- frontend/src/app/services/areasdetrabajo.service.ts
+++ frontend/src/app/services/areasdetrabajo.service.ts
@@ -9,6 +9,10 @@ export class AreasDeTrabajoService {
   areas: Array<AreaDeTrabajo> = [];
 
   constructor(private http: HttpClient) {
+    this.refresh();
+  }
+
+  refresh() {
     this.http.get<AreaDeTrabajo[]>("http://localhost:3000/api/areasdetrabajo").subscribe(response => {
       this.areas = response;
     });
@@ -18,12 +22,42 @@ export class AreasDeTrabajoService {
     return this.areas;
   }
 
+  getAreaDeTrabajoById(id: number) {
+    let search = this.areas.find(area => area.id == id);
+    return search;
+  }
+
   findNameById(id: number) {
     let search = this.areas.find(area => area.id == id);
     if (search != null) {
       return search.nombre;
     }
     return "";
+  }
+
+  addAreaDeTrabajo(nombre: string) {
+    this.http.post("http://localhost:3000/api/areasdetrabajo/",{
+      "nombre": nombre
+    }).subscribe(response => {
+      console.log(response);
+      this.refresh();
+    });
+  }
+
+  updateAreaDeTrabajo(id: number, nombre: string) {
+    this.http.patch("http://localhost:3000/api/areasdetrabajo/"+id,{
+      "nombre": nombre
+    }).subscribe(response => {
+      console.log(response);
+      this.refresh();
+    });
+  }
+
+  removeById(id: number) {
+    this.http.delete("http://localhost:3000/api/areasdetrabajo/"+id).subscribe(response => {
+      console.log(response);
+      this.refresh();
+    });
   }
 
   // sleep(ms: number) {
